2e46d8d04c806ebec1afb7b37bc949fa7e3aa1d6
Ernesto Valle <evalle.mx@gmail.com>
2022-07-26 00:24:31 -0400

    Init: Elemental Mongoose CRUD Project

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv/config');
+
+// const PORT = 3000; //Using dotenv
+const app = express();
+
+//Middlewares
+// app.use('/posts', () => {
+//     console.log('This is a middleware running');
+// })
+app.use(bodyParser.json());
+app.use(cors());
+
+//import Routes
+const postsRoute = require('./routes/posts')
+
+app.use('/posts', postsRoute);
+
+//ROUTES
+app.get('/', (req, res) =>{
+    res.send('THis is home (root)');
+})
+
+/* Moved to Route */
+// app.get('/posts', (req, res) =>{  
+//     res.send('THis is posts');
+// })
+
+
+//Connect to DB
+//mongoose.connect(process.env.ATLAS_URI, { useNewUrlParser: true } () => {
+mongoose.connect(process.env.ATLAS_URI, () => {
+    console.log('Connected to Atlas');
+});
+
+
+//Start listening
+
+app.listen(process.env.PORT)
---
+++ routes/posts.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const router = express.Router();
+
+
+const Post = require('../models/Post');
+
+
+// router.get('/posts', (req, res) =>{// defined on app.js > postsRoute
+// router.get('/', (req, res) =>{
+//     res.send('We are on posts');
+// });
+
+router.get('/', async (req, res) =>{
+    try {
+        const posts = await Post.find();
+        res.json(posts);
+    } catch (error) {
+        res.json({message: error });
+    }
+});
+
+router.post('/', async (req, res) =>{
+    //console.log(req.body);  //Requires a body-parser in main
+    const post = new Post({
+        title:req.body.title,
+        description:req.body.description
+    });
+
+    try {
+        const savedPost = await post.save();
+        res.json(savedPost);    
+    } catch (error) {
+        res.json({message: error });
+    }
+    // post.save()
+    //     .then( data => {
+    //         res.status(200).json(data);
+    //     })
+    //     .catch( err => {
+    //         res.json({message: err })
+    //     });
+    //res.send('We are on posts');
+});
+
+//READ Post by Param
+router.get('/:postId', async (req, res) =>{
+     console.log(req.params.postId);
+    try {
+        const post = await Post.findById(req.params.postId)
+        res.json(post);        
+    } catch (err) {
+        res.json({message: err });
+    }
+});
+
+//Update
+router.patch('/:postId', async (req, res) =>{    
+   try {
+       const updPost = await Post.updateOne({_id:req.params.postId},
+            {
+                $set: {
+                    title:req.body.title,
+                    description:req.body.description
+                }
+            }
+         );
+       res.json(updPost);        
+   } catch (err) {
+       res.json({message: err });
+   }
+});
+
+//Delete
+router.delete('/:postId', async (req, res) =>{
+    console.log(req.params.postId);
+   try {
+       const postDel = await Post.deleteOne({_id:req.params.postId });
+       res.json(postDel);        
+   } catch (err) {
+       res.json({message: err });
+   }
+});
+module.exports = router;
